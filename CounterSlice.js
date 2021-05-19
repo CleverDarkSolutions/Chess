38db50485513f35c3cfb22ab342e5822f03ddb9f
CleverDarkSolutions <66365110+CleverDarkSolutions@users.noreply.github.com>
2021-05-19 22:15:01 +0200

    dobra dziala

--- CounterSlice.js
+++ CounterSlice.js
@@ -4,7 +4,22 @@ export const CounterSlice = createSlice({
     name: 'counter',
     initialState: {
         values : [
-            // id, background, pawn, colour, posX, posY
+            { id: 0, background: "white", pawn: "rook", colour: "black", posX: 1, posY: 8 },
+            { id: 1, background: "black", pawn: "knight", colour: "black", posX: 2, posY: 8 },
+            { id: 2, background: "white", pawn: "bishop", colour: "black", posX: 3, posY: 8 },
+            { id: 3, background: "black", pawn: "queen", colour: "black", posX: 4, posY: 8 },
+            { id: 4, background: "white", pawn: "king", colour: "black", posX: 5, posY: 8 },
+            { id: 5, background: "black", pawn: "bishop", colour: "black", posX: 6, posY: 8 },
+            { id: 6, background: "white", pawn: "knight", colour: "black", posX: 7, posY: 8 },
+            { id: 7, background: "black", pawn: "rook", colour: "black", posX: 8, posY: 8 },
+            { id: 8, background: "black", pawn: "pawn", colour: "black", posX: 1, posY: 7 },
+            { id: 9, background: "white", pawn: "pawn", colour: "black", posX: 2, posY: 7 },
+            { id: 10, background: "black", pawn: "pawn", colour: "black", posX: 3, posY: 7 },
+            { id: 11, background: "white", pawn: "pawn", colour: "black", posX: 4, posY: 7 },
+            { id: 12, background: "black", pawn: "pawn", colour: "black", posX: 5, posY: 7 },
+            { id: 13, background: "white", pawn: "pawn", colour: "black", posX: 6, posY: 7 },
+            { id: 14, background: "white", pawn: "pawn", colour: "black", posX: 7, posY: 7 },
+            { id: 15, background: "black", pawn: "pawn", colour: "black", posX: 8, posY: 7 },
         ],
         i: 0
     },
--- components/ImplementBoard.js
+++ components/ImplementBoard.js
@@ -1,29 +1,24 @@
 import Board from './Board';
 import Square from './Square';
 import { useDispatch, useSelector } from 'react-redux';
+import {useEffect} from 'react';
 import { addField, returnState } from '../CounterSlice';
 
 const ImplementBoard = () => {
     console.log(useSelector(state => state.counter.values));
     const dispatch = useDispatch();
-    for(let i=0;i<64;i++){
+    useEffect( () => { 
         console.log('log');
-        if(i==0)
-            dispatch( () => { addField( { id: i, background: "white", pawn: "rook", colour: "black", posX : 1, posY : 8 } ) });
-        else if(i==1)
-            dispatch(() => { addField({ id: i, background: "black", pawn: "knight", colour: "black", posX: 2, posY: 8 }) });
-        else if(i==2)
-            dispatch(() => { addField({ id: i, background: "white", pawn: "bishop", colour: "black", posX: 3, posY: 8 }) });
-        else if(i==3)
-            dispatch(() => { addField({ id: i, background: "black", pawn: "queen", colour: "black", posX: 4, posY: 8 }) });
-        else if(i==4)
-            dispatch(() => { addField({ id: i, background: "white", pawn: "king", colour: "black", posX: 5, posY: 8 }) });
-        else if(i==5)
-            dispatch(() => { addField({ id: i, background: "black", pawn: "bishop", colour: "black", posX: 6, posY: 8 }) });
-        else if(i==6)
-            dispatch(() => { addField({ id: i, background: "white", pawn: "knight", colour: "black", posX: 7, posY: 8 }) });
-        else if(i==7)
-            dispatch(() => { addField({ id: i, background: "black", pawn: "rook", colour: "black", posX: 8, posY: 8 }) });
+        
+            dispatch( () => { addField( { id: 0, background: "white", pawn: "rook", colour: "black", posX : 1, posY : 8 } ) });
+            dispatch(() => { addField({ id: 1, background: "black", pawn: "knight", colour: "black", posX: 2, posY: 8 }) });
+            dispatch(() => { addField({ id: 2, background: "white", pawn: "bishop", colour: "black", posX: 3, posY: 8 }) });
+            dispatch(() => { addField({ id: 3, background: "black", pawn: "queen", colour: "black", posX: 4, posY: 8 }) });
+            dispatch(() => { addField({ id: 4, background: "white", pawn: "king", colour: "black", posX: 5, posY: 8 }) });
+            dispatch(() => { addField({ id: 5, background: "black", pawn: "bishop", colour: "black", posX: 6, posY: 8 }) });
+            dispatch(() => { addField({ id: 6, background: "white", pawn: "knight", colour: "black", posX: 7, posY: 8 }) });
+            dispatch(() => { addField({ id: 7, background: "black", pawn: "rook", colour: "black", posX: 8, posY: 8 }) });
+        /*
         else if(i==8)
             dispatch(() => { addField({ id: i, background: "black", pawn: "pawn", colour: "black", posX: 1, posY: 7 }) });
         else if(i==9)
@@ -40,14 +35,14 @@ const ImplementBoard = () => {
             dispatch(() => { addField({ id: i, background: "white", pawn: "pawn", colour: "black", posX: 7, posY: 7 }) });
         else if(i==15)
             dispatch(() => { addField({ id: i, background: "white", pawn: "pawn", colour: "black", posX: 4, posY: 7 }) });
+    };
+    */
 
 
 
 
 
-
-
-    }
+    });
     return (
         <Board arrayPawns={useSelector( state => state.counter.values)}></Board>
         //<button onClick={}></button>
